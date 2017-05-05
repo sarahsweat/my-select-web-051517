189ede214fe111048a16a6d65099f862e6c215bc
Sarah Sweat <sarahsweat@gmail.com>
2017-05-05 21:06:38 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+    i = 1
+    y = 1
+    selected = []
+    while i <= array.size
+      if yield(array[i-1]) == true
+        selected[y-1] = array[i-1]
+        y += 1
+      end
+      i += 1
+    end
+    selected
 end
